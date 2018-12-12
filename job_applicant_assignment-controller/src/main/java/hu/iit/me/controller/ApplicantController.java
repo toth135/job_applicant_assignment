e42edace734cf7adf4d69e9d3aacb196c6987815
Toth Peter <toth.peter618@gmail.com>
2018-12-13 00:35:54 +0100

    Filtering requests have been modified.

--- job_applicant_assignment-controller/src/main/java/hu/iit/me/controller/ApplicantController.java
+++ job_applicant_assignment-controller/src/main/java/hu/iit/me/controller/ApplicantController.java
@@ -2,6 +2,7 @@ package hu.iit.me.controller;
 
 import hu.iit.me.dto.ApplicantDTO;
 import hu.iit.me.dto.Converter;
+import hu.iit.me.model.Applicant;
 import hu.iit.me.model.Qualification;
 import hu.iit.me.service.ApplicantService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class ApplicantController {
@@ -22,31 +24,42 @@ public class ApplicantController {
 
     @GetMapping(value="/applicants/all")
     public List<ApplicantDTO> getApplicants() {
-        return applicantService.getApplicants().stream()
-                .map(Converter::convertApplicantToDTO)
-                .collect(Collectors.toList());
+        Collection<Applicant> applicants = this.applicantService.getApplicants();
+        List<ApplicantDTO> applicantDTOS = new ArrayList<>();
+        for(Applicant applicant : applicants) {
+            applicantDTOS.add(Converter.convertApplicantToDTO(applicant));
+        }
+        return applicantDTOS;
     }
 
     @GetMapping("/applicants/byqualification/{qualification}")
     public List<ApplicantDTO> getApplicantsByQualification(@PathVariable Qualification qualification) {
-        return applicantService.getApplicantByQualification(qualification).stream()
-                .map(Converter::convertApplicantToDTO)
-                .collect(Collectors.toList());
+        Collection<Applicant> applicants = this.applicantService.getApplicantByQualification(qualification);
+        List<ApplicantDTO> applicantDTOS = new ArrayList<>();
+        for(Applicant applicant : applicants) {
+            applicantDTOS.add(Converter.convertApplicantToDTO(applicant));
+        }
+        return applicantDTOS;
     }
 
     @GetMapping("/applicants/byfirstname/{firstName}")
     public List<ApplicantDTO> getApplicantsByFirstName(@PathVariable String firstName) {
-        return applicantService.getApplicantByFirstName(firstName).stream()
-                .map(Converter::convertApplicantToDTO)
-                .collect(Collectors.toList());
-
+        Collection<Applicant> applicants = this.applicantService.getApplicantByFirstName(firstName);
+        List<ApplicantDTO> applicantDTOS = new ArrayList<>();
+        for(Applicant applicant : applicants) {
+            applicantDTOS.add(Converter.convertApplicantToDTO(applicant));
+        }
+        return applicantDTOS;
     }
 
     @GetMapping("/applicants/bybirthdate/{birthDate}")
     public List<ApplicantDTO> getApplicantsByBirthDate(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date birthDate) {
-        return applicantService.getApplicantByBirthDate(birthDate).stream()
-                .map(Converter::convertApplicantToDTO)
-                .collect(Collectors.toList());
+        Collection<Applicant> applicants = this.applicantService.getApplicantByBirthDate(birthDate);
+        List<ApplicantDTO> applicantDTOS = new ArrayList<>();
+        for(Applicant applicant : applicants) {
+            applicantDTOS.add(Converter.convertApplicantToDTO(applicant));
+        }
+        return applicantDTOS;
     }
 
 }
--- job_applicant_assignment-controller/src/main/java/hu/iit/me/controller/JobController.java
+++ job_applicant_assignment-controller/src/main/java/hu/iit/me/controller/JobController.java
@@ -2,6 +2,7 @@ package hu.iit.me.controller;
 
 import hu.iit.me.dto.Converter;
 import hu.iit.me.dto.JobDTO;
+import hu.iit.me.model.Job;
 import hu.iit.me.model.Qualification;
 import hu.iit.me.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class JobController {
@@ -20,30 +22,42 @@ public class JobController {
 
     @GetMapping(value = "/jobs/all")
     public List<JobDTO> getJobs() {
-        return jobService.getJobs().stream()
-                .map(Converter::convertJobToDTO)
-                .collect(Collectors.toList());
+        Collection<Job> jobs = this.jobService.getJobs();
+        List<JobDTO> jobDTOS = new ArrayList<>();
+        for(Job job : jobs) {
+            jobDTOS.add(Converter.convertJobToDTO(job));
+        }
+        return jobDTOS;
     }
 
     @GetMapping("/jobs/bycategory/{category}")
     public List<JobDTO> getJobsByCategory(@PathVariable String category) {
-        return jobService.getJobByCategory(category).stream()
-                .map(Converter::convertJobToDTO)
-                .collect(Collectors.toList());
+        Collection<Job> jobs  = this.jobService.getJobByCategory(category);
+        List<JobDTO> jobDTOS = new ArrayList<>();
+        for(Job job : jobs) {
+            jobDTOS.add(Converter.convertJobToDTO(job));
+        }
+        return jobDTOS;
     }
 
     @GetMapping("/jobs/bypayment/{payment}")
     public List<JobDTO> getJobsByPayment(@PathVariable Integer payment) {
-        return jobService.getJobByPayment(payment).stream()
-                .map(Converter::convertJobToDTO)
-                .collect(Collectors.toList());
+        Collection<Job> jobs  = this.jobService.getJobByPayment(payment);
+        List<JobDTO> jobDTOS = new ArrayList<>();
+        for(Job job : jobs) {
+            jobDTOS.add(Converter.convertJobToDTO(job));
+        }
+        return jobDTOS;
     }
 
     @GetMapping("jobs/byrequiredqualification/{requiredQualification}")
     public List<JobDTO> getJobsByRequiredQualification(@PathVariable Qualification requiredQualification) {
-        return jobService.getJobByRequiredQualification(requiredQualification).stream()
-                .map(Converter::convertJobToDTO)
-                .collect(Collectors.toList());
+        Collection<Job> jobs  = this.jobService.getJobByRequiredQualification(requiredQualification);
+        List<JobDTO> jobDTOS = new ArrayList<>();
+        for(Job job : jobs) {
+            jobDTOS.add(Converter.convertJobToDTO(job));
+        }
+        return jobDTOS;
     }
 
 
